07b999cc63fa7e2e783e005f55b3005f6f50fef9
panjamapon karnasuta <panjamaponkarnasuta@panjamapons-MacBook-Pro.local>
2024-03-23 16:23:57 +0700

    Add API route for features/server.js

--- api/router/supplier_delivery/index.js
+++ api/router/supplier_delivery/index.js
@@ -1,14 +1,17 @@
 import express from 'express';
 const supplier_api = express();
 
-// Route 1
 supplier_api.get("/test", (req, res) => {
-  res.send("Hello world");
+  
 });
+supplier_api.post("/route2", (req, res) => {
 
-// Route 2
-supplier_api.get("/route2", (req, res) => {
-  res.send("This is Route 2");
+});
+supplier_api.put("/route2", (req, res) => {
+
+});
+supplier_api.delete("/route2", (req, res) => {
+
 });
 
 // Export the router
