34f21479c025e801dc7a1d0b8fd110330899e4d7
panjamapon <panjamapon.ka@yipintsoi.com>
2024-06-23 14:29:07 +0700

    evaluateNewUI

--- src/pages/evaluate_form/components/index.jsx
+++ src/pages/evaluate_form/components/index.jsx
@@ -37,7 +37,7 @@ import {
 import schema from "../../../javascript/print_schema";
 import convert_year_th from "../../../javascript/convert_year_th";
 import generatePDF from "../../../javascript/generate_pdf/evaluation_pdf/generate_pdf";
-import { CloseCircleOutlined } from "@ant-design/icons";
+import { CloseCircleOutlined, FilePdfOutlined } from "@ant-design/icons";
 
 const dateFormat = "DD/MM/YYYY";
 
@@ -356,8 +356,24 @@ const Evaluate = (props) => {
               />
             </Form.Item>
             <Form.Item>
-              <Button htmlType="submit" className="table m-auto">
-                UPDATE EVALUATE
+              <Button htmlType="submit" className="table m-auto uppercase">
+                <div className="flex flex-row">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="size-5 mr-2"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10"
+                    />
+                  </svg>
+                  SAVE draft
+                </div>
               </Button>
             </Form.Item>
           </div>
@@ -433,7 +449,23 @@ const Evaluate = (props) => {
             </Form.Item>
             <Form.Item>
               <Button htmlType="submit" className="table m-auto uppercase">
-                Approve
+                <div className="flex flex-row">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="size-5 mr-2"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M9 12.75 11.25 15 15 9.75M21 12c0 1.268-.63 2.39-1.593 3.068a3.745 3.745 0 0 1-1.043 3.296 3.745 3.745 0 0 1-3.296 1.043A3.745 3.745 0 0 1 12 21c-1.268 0-2.39-.63-3.068-1.593a3.746 3.746 0 0 1-3.296-1.043 3.745 3.745 0 0 1-1.043-3.296A3.745 3.745 0 0 1 3 12c0-1.268.63-2.39 1.593-3.068a3.745 3.745 0 0 1 1.043-3.296 3.746 3.746 0 0 1 3.296-1.043A3.746 3.746 0 0 1 12 3c1.268 0 2.39.63 3.068 1.593a3.746 3.746 0 0 1 3.296 1.043 3.746 3.746 0 0 1 1.043 3.296A3.745 3.745 0 0 1 21 12Z"
+                    />
+                  </svg>
+                  SAVE draft
+                </div>
               </Button>
             </Form.Item>
           </div>
@@ -514,7 +546,23 @@ const Evaluate = (props) => {
                             key: "action",
                             render: (record) => (
                               <Button onClick={() => openDraftDrawer(record)}>
-                                Edit
+                                <div className="flex flex-row uppercase">
+                                  <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    fill="none"
+                                    viewBox="0 0 24 24"
+                                    strokeWidth={1.5}
+                                    stroke="currentColor"
+                                    className="size-5 mr-2"
+                                  >
+                                    <path
+                                      strokeLinecap="round"
+                                      strokeLinejoin="round"
+                                      d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10"
+                                    />
+                                  </svg>
+                                  edit draft
+                                </div>
                               </Button>
                             ),
                           },
@@ -561,7 +609,23 @@ const Evaluate = (props) => {
                                     className="uppercase"
                                     onClick={() => openConfirmDrawer(record)}
                                   >
-                                    Approve
+                                    <div className="flex flex-row">
+                                      <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        fill="none"
+                                        viewBox="0 0 24 24"
+                                        strokeWidth={1.5}
+                                        stroke="currentColor"
+                                        className="size-5 mr-2"
+                                      >
+                                        <path
+                                          strokeLinecap="round"
+                                          strokeLinejoin="round"
+                                          d="M9 12.75 11.25 15 15 9.75M21 12c0 1.268-.63 2.39-1.593 3.068a3.745 3.745 0 0 1-1.043 3.296 3.745 3.745 0 0 1-3.296 1.043A3.745 3.745 0 0 1 12 21c-1.268 0-2.39-.63-3.068-1.593a3.746 3.746 0 0 1-3.296-1.043 3.745 3.745 0 0 1-1.043-3.296A3.745 3.745 0 0 1 3 12c0-1.268.63-2.39 1.593-3.068a3.745 3.745 0 0 1 1.043-3.296 3.746 3.746 0 0 1 3.296-1.043A3.746 3.746 0 0 1 12 3c1.268 0 2.39.63 3.068 1.593a3.746 3.746 0 0 1 3.296 1.043 3.746 3.746 0 0 1 1.043 3.296A3.745 3.745 0 0 1 21 12Z"
+                                        />
+                                      </svg>
+                                      Approve
+                                    </div>
                                   </Button>
                                 );
                               } else {
@@ -570,7 +634,10 @@ const Evaluate = (props) => {
                                     className="uppercase"
                                     onClick={savePDF.bind(this, record)}
                                   >
-                                    Save AS PDF
+                                    <div className="flex flex-row">
+                                      <FilePdfOutlined className="text-[16px] mr-2"/>
+                                      Save AS PDF
+                                    </div>
                                   </Button>
                                 ); // Or render a different button or nothing if needed
                               }
