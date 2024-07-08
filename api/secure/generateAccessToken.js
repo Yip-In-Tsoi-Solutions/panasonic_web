662061fb5432b36e88971c3054cfa620987f860f
panjamapon <panjamapon.ka@yipintsoi.com>
2024-07-08 14:49:31 +0700

    fix/generateTokenWithoutExpire

--- api/secure/generateAccessToken.js
+++ api/secure/generateAccessToken.js
@@ -2,7 +2,7 @@ const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
 function generateAccessToken(employee_id) {
-  return jwt.sign({ employee_id }, process.env.SECRET_KEY, { expiresIn: '60m' });
+  return jwt.sign({ employee_id }, process.env.SECRET_KEY);
 }
 
 module.exports = generateAccessToken;
