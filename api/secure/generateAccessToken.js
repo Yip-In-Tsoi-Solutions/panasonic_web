b9bb6e4363bf43acbb40d5e4deb160547863745d
panjamapon <panjamapon.ka@yipintsoi.com>
2024-07-25 11:04:14 +0700

    fix/TokenExpired

--- api/secure/generateAccessToken.js
+++ api/secure/generateAccessToken.js
@@ -2,7 +2,14 @@ const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
 function generateAccessToken(employee_id) {
-  return jwt.sign({ employee_id }, process.env.SECRET_KEY);
+  try {
+    return jwt.sign({ employee_id }, process.env.SECRET_KEY, {
+      expiresIn: "8h",
+    });
+  } catch (error) {
+    console.error("Error generating token:", error);
+    return null;
+  }
 }
 
 module.exports = generateAccessToken;
--- api/secure/jwt.js
+++ api/secure/jwt.js
@@ -8,7 +8,7 @@ function authenticateToken(req, res, next) {
 
   jwt.verify(token, process.env.SECRET_KEY, (err, user) => {
     if (err) {
-      return res.sendStatus(403)
+      return res.redirect('/');
     }
     req.user = user;
 
